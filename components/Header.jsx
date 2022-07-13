8ca875dd9297376662541c9164f1f0b4624ff15d
Vaibhav Khati <vaibhav.k@softcrylic.co.in>
2022-07-13 20:45:09 +0530

    Recent post and categories on homepage

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,14 +1,13 @@
-import React, { useContext } from "react";
+import React, { useState, useEffect } from "react";
 import Link from "next/link";
-import { getPosts } from "../services";
-
-const categories = [
-  { name: "React", slug: "react" },
-  { name: "Node", slug: "node" },
-  { name: "Management", slug: "management" },
-];
+import { getCategories } from "../services";
 
 const Header = () => {
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    getCategories().then((newCategories) => setCategories(newCategories));
+  }, []);
   return (
     <div className="container mx-auto px-10 mb-8">
       <div className="border-b w-full inline-block border-blue-400 py-8">
@@ -20,7 +19,7 @@ const Header = () => {
           </Link>
         </div>
         <div className="hidden md:float-left md:contents">
-          {categories.map((category, index) => (
+          {categories.map((category) => (
             <Link key={category.slug} href={`/category/${category.slug}`}>
               <span className="md:float-right mt-2 align-middle text-white ml-4  font-semibold cursor-pointer">
                 {category.name}
