ff9ed815d6aa609cd374f5f3183fda1e1d260a5a
Vaibhav Khati <vaibhav.k@softcrylic.co.in>
2022-07-18 10:51:23 +0530

    Comments, Loader, Featured Post

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,16 @@
-import type { NextPage } from "next";
 import Head from "next/head";
 import { PostCard, PostWidget, Categories } from '../components'
 import { getPosts } from '../services'
+import { FeaturedPosts } from '../sections'
 
-const Home: NextPage = ({ posts }) => {
+export default function Home({ posts }) {
   return (
     <div className="container mx-auto px-10 mb-8">
       <Head>
         <title>CMS Blog</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <FeaturedPosts />
       <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
         <div className="lg:col-span-8 col-span-1">
           {posts.map((post) => <PostCard post={post.node} key={post.title} />)}
@@ -24,8 +25,6 @@ const Home: NextPage = ({ posts }) => {
     </div>
   );
 };
-
-export default Home;
 
 export async function getStaticProps() {
   const posts = (await getPosts()) || [];
